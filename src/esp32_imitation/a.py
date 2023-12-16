397ba953b936d5da360ecf937170ed443e5e4660
Piotr Marecki <pmarecki@gmail.com>
2023-12-16 15:25:38 +0100

    feat: solve boiler problem; add some exception

--- src/esp32_imitation/a.py
+++ src/esp32_imitation/a.py
@@ -1,3 +1,6 @@
+import unittest
+
+
 def set_boiler_power(current_temp: float, desired_temp: float, max_power: float, temperature_window: float) -> float:
     """
     Funkcja obliczająca moc urządzenia grzewczego w zależności od aktualnej temepeatury (current_temp),
@@ -30,8 +33,52 @@ def set_boiler_power(current_temp: float, desired_temp: float, max_power: float,
     -max_power = 10
     - --> return = 1  (temperatura jest w 1/3 okna temperature_window)
 
-
-
     :return:
     """
-    pass
+    if temperature_window < 0:
+        raise ValueError('Temperature window must be positive!')
+
+    if current_temp > desired_temp:
+        return 0
+    if current_temp < desired_temp - temperature_window:
+        return max_power
+
+    in_range = desired_temp - current_temp  # (is > 0 for sure)
+    fraction = in_range / temperature_window  # number between 0 and 1
+
+    return fraction * max_power
+
+
+class TestBoiler(unittest.TestCase):
+
+    def test_basic(self):
+        self.assertAlmostEqual(set_boiler_power(current_temp=10, desired_temp=20, max_power=10, temperature_window=10),
+                               10, places=3)
+
+    def test_zero(self):
+        self.assertAlmostEqual(set_boiler_power(current_temp=21, desired_temp=20, max_power=10, temperature_window=10),
+                               0, places=3)
+
+    def test_linear(self):
+        self.assertAlmostEqual(set_boiler_power(current_temp=19, desired_temp=20, max_power=10, temperature_window=10),
+                               1, places=3)
+
+    def test_linear2(self):
+        self.assertAlmostEqual(set_boiler_power(current_temp=18, desired_temp=20, max_power=10, temperature_window=10),
+                               2, places=3)
+
+    def test_max2(self):
+        self.assertAlmostEqual(set_boiler_power(current_temp=-10, desired_temp=20, max_power=10, temperature_window=10),
+                               10, places=3)
+
+    def test_linear3(self):
+        self.assertAlmostEqual(set_boiler_power(current_temp=18, desired_temp=20, max_power=1, temperature_window=10),
+                               0.2, places=3)
+
+    def test_exception(self):
+        with self.assertRaises(ValueError):
+            set_boiler_power(10, 20, 10, temperature_window=-5)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ src/esp32_imitation/c.py
@@ -0,0 +1,6 @@
+x = -10
+
+
+# w ten sposób syngalizujemy w programach, że powstały sytuacje wyjątkowe (wyjątki)
+if x < 0:
+    raise ValueError('x is negative!!')

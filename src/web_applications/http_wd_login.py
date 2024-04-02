e793dc486dd9f7e568f76b87da115b4ba84c2b6d
Piotr Marecki <pmarecki@gmail.com>
2024-04-02 15:54:10 +0200

    feat: better wd-login

--- src/web_applications/http_wd_login.py
+++ src/web_applications/http_wd_login.py
@@ -1,20 +1,31 @@
+from dataclasses import dataclass
 from hashlib import md5
 import requests
 
-WD_AUTHENTICATION = "https://wdauth.wsi.edu.pl/"
+WD_AUTHENTICATION_URL = "https://wdauth.wsi.edu.pl/"
 
 
-def get_wd_token(user_id: int, password: str) -> str:
+@dataclass
+class Token:
+    studentid: int
+    wdauth: str
+    expiry_epoch_s: int
+
+
+def get_wd_token(user_id: str, password: str) -> tuple[Token, list[str]]:
     pass_md5 = md5(password.encode('utf-8')).hexdigest()
-    token_url = WD_AUTHENTICATION + "auth?album=" + str(user_id) + "&pass=" + pass_md5
     # Alternatywnie
-    # token_url = f'{WD_AUTHENTICATION}auth?album={str(user_id)}&pass={pass_md5}'
+    token_url = f'{WD_AUTHENTICATION_URL}authenticate?album={user_id}&pass={pass_md5}'
     resp = requests.get(token_url)
     if resp.status_code == 200:
-        token = resp.text
-        return token
-
+        data = resp.json()
+        token = data['token']
+        roles = data['roles']
+        return token, roles
+    else:
+        raise RuntimeError('Unauthorized')
 
 
 if __name__ == '__main__':
-    pass
+    token_ = get_wd_token('1234', '--secret--')
+    print(token_)

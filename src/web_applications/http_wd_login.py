825fa0b390d4558ee9010bd27802e9cd0c99ad37
Piotr Marecki <pmarecki@gmail.com>
2024-04-02 16:04:00 +0200

    feat: add `get_roles` from WD

--- src/web_applications/http_wd_login.py
+++ src/web_applications/http_wd_login.py
@@ -1,8 +1,9 @@
 from dataclasses import dataclass
 from hashlib import md5
+
 import requests
 
-WD_AUTHENTICATION_URL = "https://wdauth.wsi.edu.pl/"
+WD_AUTHENTICATION_URL = "https://wdauth.wsi.edu.pl"
 
 
 @dataclass
@@ -15,17 +16,29 @@ class Token:
 def get_wd_token(user_id: str, password: str) -> tuple[Token, list[str]]:
     pass_md5 = md5(password.encode('utf-8')).hexdigest()
     # Alternatywnie
-    token_url = f'{WD_AUTHENTICATION_URL}authenticate?album={user_id}&pass={pass_md5}'
+    token_url = f'{WD_AUTHENTICATION_URL}/authenticate?album={user_id}&pass={pass_md5}'
     resp = requests.get(token_url)
     if resp.status_code == 200:
         data = resp.json()
-        token = data['token']
+        token = Token(**data['token'])
         roles = data['roles']
         return token, roles
     else:
         raise RuntimeError('Unauthorized')
 
 
+def get_roles(token: str) -> list[str]:
+    url = f'{WD_AUTHENTICATION_URL}/roles?wdauth={token}'
+    resp = requests.get(url)
+    if resp.status_code == 200:
+        role_response = resp.json()
+        return [r['nazwa'] for r in role_response]
+    else:
+        raise RuntimeError('Unauthorized')
+
+
 if __name__ == '__main__':
-    token_ = get_wd_token('1234', '--secret--')
+    token_, roles_ = get_wd_token('4444', '--secret--')
     print(token_)
+    roles = get_roles(token_.wdauth)
+    print(roles)

9a83768fefd353a841f0e45586f5b23b7bac63a1
Piotr Marecki <pmarecki@gmail.com>
2024-04-27 14:12:17 +0200

    feat: log2 binary search impl

--- src/bin_search/log2.py
+++ src/bin_search/log2.py
@@ -2,4 +2,18 @@ from math import log
 
 
 def get_log_2(x: float) -> float:
-    return log(x, 2)
+    if x <= 0:
+        return 0
+    mi = -30
+    mx = 30
+    while True:
+        y = (mx + mi) / 2
+        if 2 ** y < x:
+            mi = y
+        else:
+            mx = y
+        if mi + 10 ** -5 >= mx:
+            break
+    # print(min, max)
+    # print(2 ** min,2 ** max)
+    return y

1125501cf1eb1315b10a63988114d56c310c23bc
Piotr Marecki <pmarecki@gmail.com>
2024-05-18 14:18:44 +0200

    feat: tests for find missing parts

---
+++ src/missing_parts/solution_missing_parts.py
@@ -0,0 +1,2 @@
+def find_missing_and_surplus_parts(need: str, have: str) -> tuple[str, str]:
+    return ('', 'c')
---
+++ src/missing_parts/test_missing_parts.py
@@ -0,0 +1,19 @@
+from src.missing_parts.solution_missing_parts import find_missing_and_surplus_parts
+
+
+def test_simple():
+    missing, surplus = find_missing_and_surplus_parts('ab', 'b')
+    assert missing == 'a'
+    assert surplus == ''
+
+
+def test_simple2():
+    missing, surplus = find_missing_and_surplus_parts('ab', 'aab')
+    assert missing == ''
+    assert surplus == 'a'
+
+
+def test_bulk():
+    assert find_missing_and_surplus_parts('ab', 'abc') == ('', 'c')
+    assert find_missing_and_surplus_parts('aaaabbbb', 'aaaabbbc') == ('b', 'c')
+    assert find_missing_and_surplus_parts('abbc', 'abc') == ('b', '')
